ebb4227e74d3127f9d0e74e7cb72d34bccab7248
Tucker Barton <hisashiburix@gmail.com>
2021-04-03 15:55:18 -0400

    Add expressJS to project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+app.get('/') (req, res) => {
+    res.send(ReactDOM.render(<App />, document.getElementById('app')));
+}
+
+app.listen (port, () => {
+    console.log(`Running on port: ${port}`);
+});
