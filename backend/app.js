3dd969e3732f1d2e3bc442d8b55c21a4b2ef008a
Tucker Barton <hisashiburix@gmail.com>
2021-04-04 21:01:42 -0400

    Add package.json to root directory so that node server can be run from it, and thus allowing heroku to successfully deploy without issues. Update paths due to node start script running from root instead of from backend directory.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,11 +8,10 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(cors());
-app.use(logger('dev'));
 //app.use(express.json());
 
 //app.use(express.urlencoded({ extended: false }));
-app.use(express.static('../frontend/dist'))
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.use('/', indexRouter);
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.sendFile(path.resolve(__dirname,'../../frontend/dist/index.html'));
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 module.exports = router;
