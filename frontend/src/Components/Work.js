a3c8b7fddd4b4b214cf93a44fa94621367c4b1b5
Tucker Barton <hisashiburix@gmail.com>
2021-07-22 20:22:44 -0400

    Update work page. Add code_2.jpeg and work.css files to do so.

--- frontend/src/Components/Work.js
+++ frontend/src/Components/Work.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Header from './Core/Header';
 import Footer from './Core/Footer';
+import '../stylesheets/work.css';
 import Blog_Pic from '../../public/imgs/japanese_writing_pic.jpeg';
-import Code_Pic from '../../public/imgs/code_pic.jpeg';
+import Code_Pic from '../../public/imgs/code_2.jpeg';
 
 class Work extends React.Component {
     
@@ -19,14 +20,14 @@ class Work extends React.Component {
                     <div id="cv-application" class="">
                         <a href="https://github.com/Tucker-B/CV-Application">
                             <h3 className="">CV Application | Personal Site</h3>
-                            <img src={Code_Pic} className="" width="300px" height="200px"></img>
+                            <img src={Code_Pic} className=""></img>
                         </a>
                         
                     </div>
-                    <div id="nihon-kawa" class="">
-                        <a href="https://github.com/Tucker-B/nihon_kawa">
-                            <h3 className="">Nihon Kawa | Japanese Blog</h3>
-                            <img src={Blog_Pic} className=""width="300px" height="200px"></img>
+                    <div id="loop-blog" class="">
+                        <a href="https://github.com/Tucker-B/loop_blog">
+                            <h3 className="">Loop Blog | Japanese Blog</h3>
+                            <img src={Blog_Pic} className=""></img>
                         </a>
                         
                     </div>
