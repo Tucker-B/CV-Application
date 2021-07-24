7094a08049409f1a515e38500fd34a87b31717c3
Tucker Barton <hisashiburix@gmail.com>
2021-07-23 21:39:54 -0400

    Finalize some things, like updating wording of paragraphs on index.js, giving said p's some margin on the bottom, etc.

--- frontend/src/Components/Index.js
+++ frontend/src/Components/Index.js
@@ -25,13 +25,13 @@ class Index extends React.Component {
                     <div id="bio-description" className="row">
                         <div className="bio-description__p-div">
                             <h3 id="bio-description__yokoso">ようこそ！</h3>
-                            <p className="bio-description__p"><span className="green-text">Hi, I'm Tucker!</span> I am an aspiring web developer located in the Metro Atlanta area in the wonderful state of Georgia, US. In addition, I have the great honor of attending the <i className="green-text">University of Georgia</i> at which I am a senior learning 日本語, or Japanese. </p>
+                            <p className="bio-description__p"><span className="green-text">Hi, I'm Tucker!</span> I am a web developer located in the Metro Atlanta area in the wonderful state of Georgia, US. I am attending the <i className="green-text">University of Georgia</i> at which I am a senior learning Japanese. I will be graduating this fall of 2021! </p>
 
-                            <p className="bio-description__p">I have been interested in computers for as long as I can remember, always tinkering with my gadgets and wondering how things actually worked.</p>
+                            <p className="bio-description__p">I have been interested in computers for as long as I can remember, always tinkering with my surroundings and wondering how things actually worked.</p>
 
-                            <p className="bio-description__p">When I learned of the world of software development, I immediately knew that was what my career going forward would be.</p>
+                            <p className="bio-description__p">When I learned of the world of software development, I jumped right in and fell in love.</p>
 
-                            <p className="bio-description__p">I am currently learning technologies such as <i className="green-text">ReactJS</i>, <i className="green-text">ExpressJS</i>, and <i className="green-text">Node</i>, while also continually improving by looking to industry-wide best practices.</p>
+                            <p className="bio-description__p">I am currently learning technologies such as <i className="green-text">React.js</i>, <i className="green-text">Express.js</i>, and <i className="green-text">Node</i>, while also continually improving by looking to leading developers and thinkers of the industry.</p>
 
                         </div>
                         
